0e64d02a6f71a536aec0f69cc76235a6835e371a
Imashi Kinigama <78595183+timelesswanderder24@users.noreply.github.com>
2022-08-10 19:44:59 +1200

    first commit

--- TestProject1/UnitTest1.cs
+++ TestProject1/UnitTest1.cs
@@ -1,16 +1,52 @@
+using myAPI.Controllers;
+using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
 namespace TestProject1
 {
-    public class Tests
+    public class TeartistUnitTeststs
     {
-        [SetUp]
-        public void Setup()
+        private readonly string[] args;
+
+        [Test]
+        public void putArtist()
         {
+            //Arrange
+            var builder = WebApplication.CreateBuilder(args);
+
+            builder.Logging.ClearProviders();
+            builder.Logging.AddConsole();
+            var app = builder.Build();
+            ILogger <ArtistController> logger = app.Services.GetRequiredService<ILogger<ArtistController>>();
+            var test = new ArtistController(logger);
+
+            //Act
+            var count = test.Get().Count();
+            var actionResult = test.Post("Jane", 13, "Rock");
+            //Assert
+            Assert.AreEqual(count, test.Get().Count());
+
         }
 
         [Test]
-        public void Test1()
+        public void deleteArtist()
         {
-            Assert.Pass();
+            //Arrange
+            var builder = WebApplication.CreateBuilder(args);
+
+            builder.Logging.ClearProviders();
+            builder.Logging.AddConsole();
+            var app = builder.Build();
+            ILogger<ArtistController> logger = app.Services.GetRequiredService<ILogger<ArtistController>>();
+            var test = new ArtistController(logger);
+
+            //Act
+            var count = test.Get().Count();
+            var actionResult = test.DemonstrateDelete(test.Get().ElementAt(0).Name);
+            //Assert
+            Assert.AreEqual(count, test.Get().Count());
+
         }
     }
 }
